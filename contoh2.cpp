f810f29a33f9029a3c444d1532b7ed6a37b6dc28
Irvan Fikriansyah <fikriansyah.irvan@gmail.com>
2017-03-24 15:09:07 +0700

    Add files via upload
    Tugas ke2 Algotitma pemrograman (IRVAN FIKRIANSYAH, TIF RM 16B, 16111206)

---
+++ contoh2.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+	cout << "Hello World \n";
+		cout << "Hello World plus enter" << endl;
+			return 0;
+}
---
+++ contoh4.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+	int x;
+		cout << "Masukkan nilai X : ";
+		cin >> x;
+		return 0;
+}
