c84a1cccca38f7b392ed24edd8aafa9cde7cc803
Irvan Fikriansyah <fikriansyah.irvan@gmail.com>
2017-03-31 23:21:42 +0700

    Pertemuan 4

---
+++ latihan2_Pertemuan4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int perbandingan (int a, int b);
+int main () {
+	int nilai1, nilai2;
+	cout << "masukkan nilai a: ";
+	cin >> nilai1;
+	cout << "masukkan nilai b: ";
+	cin >> nilai2;
+	cout << "nilai maksimal : " << perbandingan (nilai1, nilai2);
+}
+
+int perbandingan (int a, int b){
+	return max (a,b);
+}
