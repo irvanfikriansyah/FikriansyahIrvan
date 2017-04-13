42fa8cec8ac84859308cf4c9d943944b4641730b
Irvan Fikriansyah <fikriansyah.irvan@gmail.com>
2017-04-13 13:09:37 +0700

    Add files via upload
    Pertemuan 6

---
+++ Pertemuan6_Latihan2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+
+int main () {
+//kamus
+string nama1, nama2, nama3, nama4, nama5, nama6, nama7, nama8, nama9, nama10;
+//algoritma
+cin >> nama1;
+cin >> nama2;
+cin >> nama3;
+cin >> nama4;
+cin >> nama5;
+cin >> nama6;
+cin >> nama7;
+cin >> nama8;
+cin >> nama9;
+cin >> nama10;
+
+cout << nama1 << "-" << nama2 << endl;
+cout << nama1 << "-" << nama3 << endl;
+cout << nama1 << "-" << nama4 << endl;
+cout << nama1 << "-" << nama5 << endl;
+cout << nama1 << "-" << nama6 << endl;
+cout << nama2 << "-" << nama7 << endl;
+cout << nama2 << "-" << nama8 << endl;
+cout << nama2 << "-" << nama10<< endl;
+cout << nama2 << "-" << nama9 << endl;
+cout << nama2 << "-" << nama4 << endl;
+
+}
